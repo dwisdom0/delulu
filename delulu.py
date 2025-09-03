0238630b3eded44a08775ac2d3cab9babf822df5
dwisdom0 <2480711+dwisdom0@users.noreply.github.com>
2025-09-03 13:14:41 -0700

    Run multiple trials of each training run to get a confidence interval

--- delulu.py
+++ delulu.py
@@ -4,13 +4,20 @@ import torch.nn as nn
 import torch.optim as optim
 import torchvision.transforms.v2 as transforms
 import plotly.graph_objects as go
+import plotly.express as px
 from torchvision import datasets
 from torch.utils.data import DataLoader
 from dataclasses import dataclass
 from multiprocessing import Pool, cpu_count
+from tqdm import tqdm
+from scipy.stats import bootstrap
 
 from load_penguins import load_penguins_datasets
 from load_moons import load_moons_datasets
+
+import warnings
+
+warnings.simplefilter("error")
 
 
 @dataclass
@@ -142,18 +149,19 @@ def train(
             data, labels = data.to(device), labels.to(device)
 
             outputs = model(data)
+            if torch.isnan(outputs).any():
+                breakpoint()
             # labels are 1D batch_size but torch CrossEntropyLoss automatically
             # interprets them as class indicies
             loss = loss_func(outputs, labels)
+            if torch.isnan(loss).any():
+                breakpoint()
             epoch_loss += loss.item()
 
             optimizer.zero_grad()
             loss.backward()
             optimizer.step()
 
-        print(
-            f"Epoch [{epoch + 1}/{epochs}], Mean Loss: {epoch_loss / len(train_dataloader):.4f}"
-        )
         mean_train_losses.append(epoch_loss / len(train_dataloader))
         test_accys.append(evaluate(model, test_dataloader))
     return mean_train_losses, test_accys
@@ -182,30 +190,15 @@ def run_experiment(
     train_dataloader: DataLoader,
     test_dataloader: DataLoader,
     name: str,
+    num_trials: int = 5,
 ):
-    print(f"\nRunning {name}\n")
-    train_args_list = []
-    for act_func in funcs:
-        model = MLP(
-            input_dim=mlp_config.input_dim,
-            hidden_dims=mlp_config.hidden_dims,
-            output_dim=mlp_config.output_dim,
-            activation_func=act_func,
-        )
-        train_args_list.append(
-            (model, train_dataloader, test_dataloader, mlp_config.epochs)
-        )
-
-    with Pool(cpu_count()) as pool:
-        results = pool.starmap(train, train_args_list)
-
     train_fig = go.Figure()
     train_fig.update_layout(
         yaxis_title="Mean Training Loss",
         xaxis_title="Epoch",
     )
     train_fig.update_layout(
-        title_text=f"Training loss on {name} for different activation functions"
+        title_text=f"Training loss on {name} for different activation functions ({num_trials=})"
     )
 
     test_fig = go.Figure()
@@ -214,23 +207,117 @@ def run_experiment(
         xaxis_title="Epoch",
     )
     test_fig.update_layout(
-        title_text=f"Test accuracy on {name} for different activation functions"
+        title_text=f"Test accuracy on {name} for different activation functions ({num_trials=})"
     )
 
-    for result, act_func in zip(results, funcs):
+    print(f"\nRunning {name}\n")
+    for i, act_func in tqdm(enumerate(funcs), ascii=True, total=len(funcs)):
+        train_losses = []
+        test_accys = []
+        train_args_list = []
+        results = []
+        for trial in tqdm(range(num_trials), ascii=True, leave=False):
+            model = MLP(
+                input_dim=mlp_config.input_dim,
+                hidden_dims=mlp_config.hidden_dims,
+                output_dim=mlp_config.output_dim,
+                activation_func=act_func,
+            )
+            results.append(
+                train(model, train_dataloader, test_dataloader, mlp_config.epochs)
+            )
+        #    train_args_list.append(
+        #        (model, train_dataloader, test_dataloader, mlp_config.epochs)
+        #    )
+        # with Pool(cpu_count()) as pool:
+        #    results = pool.starmap(train, train_args_list)
+
+        for result in results:
+            train_losses.append(result[0])
+            test_accys.append(result[1])
+
+        train_losses = np.array(train_losses)
+        test_accys = np.array(test_accys)
+
+        mean_train, hi_train, lo_train = many_runs_to_mean_plus_ci(train_losses)
+        mean_test, hi_test, lo_test = many_runs_to_mean_plus_ci(test_accys)
+
         try:
             func_name = act_func.name()
         except AttributeError:
             func_name = str(act_func().__class__)
+
+        func_color = px.colors.qualitative.Plotly[i]
         train_losses, test_accys = result
         train_fig.add_trace(
-            go.Scatter(x=list(range(len(train_losses))), y=train_losses, name=func_name)
+            go.Scatter(
+                x=list(range(len(mean_train))),
+                y=mean_train.tolist(),
+                name=func_name,
+                line=dict(color=func_color),
+            )
+        )
+        train_fig.add_trace(
+            go.Scatter(
+                x=list(range(len(hi_train))) + list(range(len(hi_train)))[::-1],
+                y=hi_train.tolist() + lo_train.tolist()[::-1],
+                name=f"{func_name} CI",
+                fill="toself",
+                fillcolor=hex_to_rgba(func_color, 0.2),
+                line=dict(color=hex_to_rgba(func_color, 0)),
+                hoverinfo="skip",
+                showlegend=True,
+            )
         )
         test_fig.add_trace(
-            go.Scatter(x=list(range(len(test_accys))), y=test_accys, name=func_name)
+            go.Scatter(
+                x=list(range(len(mean_test))),
+                y=mean_test.tolist(),
+                name=func_name,
+                line=dict(color=func_color),
+            )
         )
+        test_fig.add_trace(
+            go.Scatter(
+                x=list(range(len(hi_test))) + list(range(len(hi_test)))[::-1],
+                y=hi_test.tolist() + lo_test.tolist()[::-1],
+                name=f"{func_name} CI",
+                fill="toself",
+                fillcolor=hex_to_rgba(func_color, 0.2),
+                line=dict(color=hex_to_rgba(func_color, 0)),
+                hoverinfo="skip",
+                showlegend=True,
+            )
+        )
+
     train_fig.show()
     test_fig.show()
+
+
+def many_runs_to_mean_plus_ci(data: np.array):
+    """ "
+    expects an array with shape (num_runs, run)
+    """
+    if np.isnan(data).any():
+        breakpoint()
+    mean = np.mean(data, axis=0)
+    try:
+        bca_ci = bootstrap(
+            (data,), statistic=np.mean, vectorized=True, axis=0, method="BCa"
+        )
+    except:
+        breakpoint()
+    hi = bca_ci.confidence_interval.high
+    lo = bca_ci.confidence_interval.low
+    # breakpoint()
+    return mean, hi, lo
+
+
+def hex_to_rgba(h: str, alpha: float = 0.2):
+    r = int(h[1:3], base=16)
+    g = int(h[3:5], base=16)
+    b = int(h[5:7], base=16)
+    return f"rgba({r}, {g}, {b}, {alpha})"
 
 
 if __name__ == "__main__":
@@ -260,7 +347,7 @@ if __name__ == "__main__":
     test_penguins = DataLoader(test_penguins, batch_size=1024)
 
     penguins_model_config = MLPConfig(
-        input_dim=7, hidden_dims=[64, 16], output_dim=3, epochs=10
+        input_dim=7, hidden_dims=[16, 64, 16], output_dim=3, epochs=10
     )
 
     run_experiment(
